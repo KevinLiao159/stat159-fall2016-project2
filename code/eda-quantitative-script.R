b17eeea6b1da92d4ad7dacf0cb530cc4cabca885
Kevin Liao <lwk723@berkeley.edu>
2016-10-26 22:34:06 -0700

    Modified file: eda-quantitative-script.R

--- code/eda-quantitative-script.R
+++ code/eda-quantitative-script.R
@@ -26,77 +26,22 @@ sink()
 # Create and save histograms and boxplots of all quantitative variables in PNG format
 # Use for loop to create and save histogram and boxplot of each quantitative variable
 for (quant_var in c("Income", "Limit", "Rating", "Cards", "Age", "Education", "Balance")) {
+  # Create and save histograms
   png(paste0("images/histogram-", quant_var, ".png"))
-  hist(unlist(credit[quant_var]), breaks=10, main = c("Histogram of Income", xlab = quant_var)
-  
+  hist(unlist(credit[quant_var]), breaks=10, main = paste("Histogram of", quant_var), xlab = quant_var)
+  dev.off()
+  # Create and save boxplots
+  png(paste0("images/boxplot-", quant_var, ".png"))
+  boxplot(credit[quant_var], main = paste("Boxplot of", quant_var), xlab = quant_var)
+  dev.off()
 }
 
 
+# Save correlation matrix as RData
+corr_matrix <- cor(credit[, c(1 : 6, 11)])
+save(corr_matrix, file = "data/correlation-matrix.RData")
 
-png("images/histogram-income.png")
-hist(credit$Income, breaks=10, main="Histogram of Income", xlab="Income")
-dev.off()
-
-
-png("images/histogram-limit.png")
-hist(credit$Limit, breaks=10, main="Histogram of Limit", xlab="Limit")
-dev.off()
-
-png("images/histogram-rating.png")
-hist(credit$Rating, breaks=10, main="Histogram of Rating", xlab="Rating")
-dev.off()
-
-png("images/histogram-cards.png")
-hist(credit$Cards, breaks=10, main="Histogram of No. of Cards", xlab="No. of Cards")
-dev.off()
-
-png("images/histogram-age.png")
-hist(credit$Age, breaks=10, main="Histogram of Age", xlab="Age")
-dev.off()
-
-png("images/histogram-education.png")
-hist(credit$Education, breaks=10, main="Histogram of Education", xlab="Education")
-dev.off()
-
-png("images/histogram-balance.png")
-hist(credit$Balance, breaks=10, main="Histogram of Balance", xlab="Balance")
-dev.off()
-
-#Create images for boxplots of quantitative variables
-
-png("images/boxplot-income.png")
-boxplot(credit$Income, main="Boxplot of Income", xlab="Income")
-dev.off()
-
-png("images/boxplot-limit.png")
-boxplot(credit$Limit, main="Boxplot of Limit", xlab="Limit")
-dev.off()
-
-png("images/boxplot-rating.png")
-boxplot(credit$Rating, main="Boxplot of Rating", xlab="Rating")
-dev.off()
-
-png("images/boxplot-cards.png")
-boxplot(credit$Cards, main="Boxplot of No. of Cards", xlab="No. of Cards")
-dev.off()
-
-png("images/boxplot-age.png")
-boxplot(credit$Age, main="Boxplot of Age", xlab="Age")
-dev.off()
-
-png("images/boxplot-education.png")
-boxplot(credit$Education, main="Boxplot of Education", xlab="Education")
-dev.off()
-
-png("images/boxplot-balance.png")
-boxplot(credit$Education, main="Boxplot of Balance", xlab="Balance")
-dev.off()
-
-#Save correlation matrix as RData
-corr_matrix <- cor(credit[, c(2:7,12)])
-save(corr_matrix, file = 'data/correlation-matrix.RData')
-
-#Create image for scatterplot matrix
+# Create image for scatterplot matrix
 png("images/scatterplot-matrix.png")
-pairs(credit[,c(2:7,12)])
+pairs(credit[,c(1 : 6, 11)])
 dev.off()
