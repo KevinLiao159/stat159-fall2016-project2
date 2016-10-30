b12f5373c1fc65d9733cf067aa5d8f4f56f280d3
Kevin Liao <lwk723@berkeley.edu>
2016-10-30 15:07:31 -0700

    Finished ridge-regresssion-script.R

--- code/scripts/ridge-regression-script.R
+++ code/scripts/ridge-regression-script.R
@@ -24,11 +24,11 @@ ridge_cv_plot <- plot(ridge_cv)
 ridge_cv_plot
 
 # Find the lambda for the best model
-bestlam <- ridge_cv$lambda.min
-bestlam
+ridge_bestlam <- ridge_cv$lambda.min
+ridge_bestlam
 
 # Use the test set to compute the test Mean Square Error (test MSE)
-ridge_pred <- predict(ridge_cv, s = bestlam, newx = x_matrix[test ,], 
+ridge_pred <- predict(ridge_cv, s = ridge_bestlam, newx = x_matrix[test ,], 
                       intercept = FALSE, standardize = FALSE)
 # Load mse function
 source("code/functions/mse-function.R")
@@ -36,12 +36,26 @@ ridge_test_mse <- mse(ridge_pred, y_vector[test])
 ridge_test_mse
 
 # Refit the model on the full data set using the best lambda and get official coefficients
-ridge_model <- glmnet(x_matrix, y_vector, alpha = 0, lambda = bestlam, 
+ridge_model <- glmnet(x_matrix, y_vector, alpha = 0, lambda = ridge_bestlam, 
                       intercept = FALSE, standardize = FALSE)
 
 ridge_coef <- coef(ridge_model)
 ridge_coef
 
-# Save cross-validation in binary
+# Save cv output model, best lambda, test MSE, best model, 
+# and official coefficients in a binary file
+save(ridge_cv, ridge_bestlam, ridge_test_mse, ridge_model, ridge_coef, 
+     file = "data/ridge-regression.RData")
 
-# save ,.... in txt 
+
+# Save cv output model, best lambda, test MSE, best model, 
+# and official coefficients in text file
+sink("data/ridge-regression-output.txt")
+cat("Results of ridge regression", "\n")
+cat("The best lambda from 10-fold cross validation:", ridge_bestlam, "\n", append = TRUE)
+cat("\n", append = TRUE)
+cat("Test Mean Square Error from best model:", ridge_test_mse, "\n", append = TRUE)
+cat("\n", append = TRUE)
+cat("Official coefficients from best model", "\n", append = TRUE)
+ridge_coef
+sink()
