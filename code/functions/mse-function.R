278a1c8eeebca6d4f7353d1c5ca5d5553e5f58bb
Kevin Liao <lwk723@berkeley.edu>
2016-11-01 14:53:30 -0700

    Modified mse-function.R: add description

--- code/functions/mse-function.R
+++ code/functions/mse-function.R
@@ -1,4 +1,9 @@
-# MSE function
+# Title: MSE function
+# Description: Compute Mean Square Error for Models
+# Inputs: Fitted values, Testing values
+# Output: Mean squre error for model
+
+
 mse <- function(fitted_values, test_value) {
   mse = mean((as.numeric(fitted_values) - as.numeric(test_value)) ^ 2)
   return(mse)
